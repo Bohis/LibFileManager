de6063b403688dc71f83e442477a22f5ef2d18b8
Борис Мурашов <murashov.b@yandex.ru>
2020-08-26 21:53:46 +0500

    Добавьте файлы проекта.

---
+++ Model/Book.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibFileManager.Model {
+	public class Book : INotifyPropertyChanged{
+		protected string _name;
+		protected int _id;
+		protected List<string> _tags;
+		protected string _author;
+		protected int _countWord;
+		protected string _description;
+		protected string _otherDescription;
+		protected string _path;
+
+		public Book() { }
+
+		public string Name {
+			get => _name;
+			set {
+				_name = value;
+				OnPropertyChanged("Name");
+			}
+		}
+
+		public int Id {
+			get => _id;
+			set {
+				_id = value;
+				OnPropertyChanged("Id");
+			}
+		}
+
+		public string Tags {
+			get {
+				string tags = "";
+
+				foreach (var tag in tags) {
+					tags += tag + " ";
+				}
+
+				return tags;
+			}
+			set {
+				_tags.Clear();
+
+				foreach (var tag in value.Split('#')) {
+					_tags.Add("#" + tag);
+				}
+
+				OnPropertyChanged("Tags");
+			}
+		}
+
+		public string Author {
+			get => _author;
+			set {
+				_author = value;
+				OnPropertyChanged("Author");
+			}
+		}
+		
+		public int CountWord {
+			get => _countWord;
+			set {
+				_countWord = value;
+				OnPropertyChanged("CountWord");
+			}
+		}
+
+		public string Description {
+			get => _description;
+			set {
+				_description = value;
+				OnPropertyChanged("Description");
+			}
+		}
+
+		public string OtherDescription {
+			get => _otherDescription;
+			set {
+				_otherDescription = value;
+				OnPropertyChanged("OtherDescription");
+			}
+		}
+
+		public string Path {
+			get => _path;
+			set {
+				_path = value;
+				OnPropertyChanged("Path");
+			}
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public void OnPropertyChanged(string prop = "") {
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+		}
+	}
+}
---
+++ View/ControlDataGridBooks.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LibFileManager.Model;
+
+namespace LibFileManager.View {
+	/// <summary>
+	/// Логика взаимодействия для ControlDataGridBooks.xaml
+	/// </summary>
+	public partial class ControlDataGridBooks : UserControl {
+		public ControlDataGridBooks() {
+			InitializeComponent();
+
+			CollectionBook = new List<Book>();
+		}
+
+		public IEnumerable<Book> CollectionBook { get; set; }
+	}
+}
